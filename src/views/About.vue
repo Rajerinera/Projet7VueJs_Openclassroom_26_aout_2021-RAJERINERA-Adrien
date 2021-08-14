e57b05e34771ccf3cfdf5d804b7debfb275c72f9
Rajerinera <adrien.rajerinera@gmail.com>
2021-08-14 12:39:38 +0200

    page about fait

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,24 +1,52 @@
 <template>
-    <div class="about">
-        <p>About us</p>
-    </div>
+  <div class="about">
+    <h1>Création du site</h1>
+    <p>
+      Creation du backend et du frontend avec une base de donnée Mysql pour le
+      projet 7 d'Openclassroom Groupomania. Ceci est le dernier projet de ma
+      formation où j'ai développé entièrement l'application du Back au Front. Ce
+      site à été réalisé en Node Js Express avec une Api rest et les règles de
+      sécurité Owasp en Back-end. Pour ce qui du Front-end, j'ai utilisé
+      boostrap HTML Css et le framework Vue.js. Le projet Groupomania est un
+      réseau social d'entreprise qui permet de partager des commentaires et des
+      images. Un admin peut également modérer le site et enfin l'utilisateur
+      peut modifier ou supprimer son profil. Installation: Installer Node Js
+      pour le Back et les dépendances npm nécessaires puis lancer la serveur
+      nodemon server ou node server. En Front le projet est en Vue CLI installer
+      Vue et ses dépendances puis lancer l'application. Vous devez au préalable
+      avoir votre base de données préparée.
+    </p>
+    <p>
+      Pour me contacter cliquer sur
+      <a href="mailto: adrien.rajerinera@live.fr">
+        adrien.rajerinera@live.fr
+      </a>
+    </p>
+
+    <p>Site crée par "Adrien Rajerinera"</p>
+    <p class="cordialement"> Cordialement!</p>
+  </div>
 </template>
 
 <script>
 export default {
-    setup() {
-        
-    },
-}
+  setup() {},
+};
 </script>
 
 <style scoped>
-    .about{
-        display: flex;
-        justify-content: center; 
-        align-items: center;
-        height: 100%;
-        width: 100%;
-        font-size: 30px;
-    }
+.about {
+  font-size: 20px;
+  font-weight: bold;
+  margin-top: 80px
+}
+p {
+  text-align: justify;
+  margin-left: auto;
+  margin-right: auto;
+  width: 80%;
+}
+.cordialement{
+    text-align: center;
+}
 </style>
