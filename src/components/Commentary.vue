391354394d3624bdc78e8478775ebfb39825c441
Rajerinera <adrien.rajerinera@gmail.com>
2021-08-14 17:02:04 +0200

    amélioration du css

--- src/components/Commentary.vue
+++ src/components/Commentary.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>
-  <button @click="getcomment">test</button>
+  <div class="media">
+    <a class="pull-left" href="#"
+      ><img
+        class="media-object"
+        src="https://bootdey.com/img/Content/avatar/avatar1.png"
+        alt=""
+    /></a>
+    <div class="media-body" v-for="comment in comments" :key="comment.idcom">
+      <h4 class="media-heading">{{comment.title}}</h4>
+      <p>{{comment.content}}</p>
+      
+  <ul class="list-unstyled list-inline media-detail pull-left">
+        <li><i class="fa fa-calendar"></i>27/02/2014</li>
+        <li><i class="fa fa-thumbs-up"></i>13</li>
+      </ul>
+      <ul class="list-unstyled list-inline media-detail pull-right">
+        <li class=""><a href="">Modifier</a></li>
+        <li class=""><a href="">Supprimer</a></li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
-import axios from "axios"
+import axios from "axios";
 //import {mapState} from "vuex";
 export default {
   name: "comment",
@@ -12,66 +30,176 @@ export default {
     return {
       title: "",
       name: "",
-      comments: []
+      comments: [],
     };
   },
   //computed:{
-    //...mapState(['comment']),
+  //...mapState(['comment']),
   //},
-  methods: {
-    getcomment: async function ()  {
+  mounted() {
     this.comments = [];
-    await axios
-      .get("http://localhost:3000/comments")
+    axios
+      .get("http://localhost:3000/api/comments", {
+        headers: {
+          Authorization: "Bearer" + localStorage.getItem("userToken"),
+        },
+      })
       .then((response) => {
-       console.log(response)
+        (this.comments = response.data), console.log(this.comments);
       })
       .catch((error) => {
         console.log(error);
       });
-    }
   },
 };
 </script>
 <style scoped>
-body {
-  padding-top: 30px;
+.media{
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  border: 1px solid black;
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
 }
-.container {
-  width: 70%;
+.media-body{
+ text-align: center;
+ border: 1px solid black;
+ margin-right: auto;
+ margin-left: auto;
+ width: 80%;
 }
-.widget .panel-body {
-  padding: 0px;
+.content-item {
+    padding:30px 0;
+	background-color:#FFFFFF;
 }
-.widget .list-group {
-  margin-bottom: 0;
+
+.content-item.grey {
+	background-color:#F0F0F0;
+	padding:50px 0;
+	height:100%;
 }
-.widget .panel-title {
-  display: inline;
+
+.content-item h2 {
+	font-weight:700;
+	font-size:35px;
+	line-height:45px;
+	text-transform:uppercase;
+	margin:20px 0;
 }
-.widget .label-info {
-  float: right;
+
+.content-item h3 {
+	font-weight:400;
+	font-size:20px;
+	color:#555555;
+	margin:10px 0 15px;
+	padding:0;
 }
-.widget li.list-group-item {
-  border-radius: 0;
-  border: 0;
-  border-top: 1px solid #ddd;
+
+.content-headline {
+	height:1px;
+	text-align:center;
+	margin:20px 0 70px;
 }
-.widget li.list-group-item:hover {
-  background-color: rgba(86, 61, 124, 0.1);
+
+.content-headline h2 {
+	background-color:#FFFFFF;
+	display:inline-block;
+	margin:-20px auto 0;
+	padding:0 20px;
 }
-.widget .mic-info {
-  color: #666666;
-  font-size: 11px;
+
+.grey .content-headline h2 {
+	background-color:#F0F0F0;
 }
-.widget .action {
-  margin-top: 5px;
+
+.content-headline h3 {
+	font-size:14px;
+	color:#AAAAAA;
+	display:block;
 }
-.widget .comment-text {
-  font-size: 12px;
+
+
+#comments {
+    box-shadow: 0 -1px 6px 1px rgba(0,0,0,0.1);
+	background-color:#FFFFFF;
 }
-.widget .btn-block {
-  border-top-left-radius: 0px;
-  border-top-right-radius: 0px;
+
+#comments form {
+	margin-bottom:30px;
 }
+
+#comments .btn {
+	margin-top:7px;
+}
+
+#comments form fieldset {
+	clear:both;
+}
+
+#comments form textarea {
+	height:100px;
+}
+
+#comments .media {
+	border-top:1px dashed #DDDDDD;
+	padding:20px 0;
+	margin:0;
+}
+
+#comments .media > .pull-left {
+    margin-right:20px;
+}
+
+#comments .media img {
+	max-width:20px;
+}
+
+#comments .media h4 {
+	margin:0 0 10px;
+}
+
+#comments .media h4 span {
+	font-size:14px;
+	float:right;
+	color:#999999;
+}
+
+#comments .media p {
+	margin-bottom:15px;
+	text-align:justify;
+}
+
+#comments .media-detail {
+	margin:0;
+}
+
+#comments .media-detail li {
+	color:#AAAAAA;
+	font-size:12px;
+	padding-right: 10px;
+	font-weight:600;
+}
+
+#comments .media-detail a:hover {
+	text-decoration:underline;
+}
+
+#comments .media-detail li:last-child {
+	padding-right:0;
+}
+
+#comments .media-detail li i {
+	color:#666666;
+	font-size:15px;
+	margin-right:10px;
+}
+img.media-object{
+  border: 1px solid black;
+  width: 50px;
+  height: 50px;
+}
+
 </style>
--- src/components/PostComment.vue
+++ src/components/PostComment.vue
@@ -1,77 +1,56 @@
 <template>
   <div>
-    <h1>N'hésitez pas à publier !</h1>
-    <div class= "form">
-    <p>{{comment.title}}</p>
-    <input
-            type="text"
-            class="bg-light form-control"
-            placeholder="Steve"
-            v-model="title1"
-          />
-    <p>{{comment.content}}</p>
-    <input
-            type="text"
-            class="bg-light form-control"
-            placeholder="Steve"
-            v-model="content1"
-          />
-    <button class="btn-btn-danger" @click="updateCom">clique pour changer</button>
-    <button class="btn-btn-primary" @click="deleteCom"> clique pour supp </button>
-    </div>
-    <!-- component envoi formulaire pour les post emis par le user connecter -->
-    <div class="d-flex justify-content-center">
-      <!-- debut check formulaire -->
-      <div
-        id="form"
-        enctype="multipart/form-data"
-        class="largeur80 l100 my-5 shadow bordurePost bordureRond"
-      >
-        <div class="form-group">
-          <label class="text-primary" for="titre">Titre du post</label>
-          <input
-            v-model="postComment.title"
-            type="text"
-            class="form-control"
-            name="title"
-            placeholder="title..."
-          />
-        </div>
-        <div class="form-group">
-          <label class="text-primary description" for="description"
-            >Description</label
-          >
-          <input
-            v-model="postComment.content"
-            class="form-control"
-            name="content"
-            rows="3"
-            placeholder="Décrire le post..."
-          />
-        </div>
-        <!-- check formulaire bien rempli et reponse serveur bouton publier -->
-        <div
-          class="form-group row d-flex align-item-center justify-content-center"
-        >
-          <div class="my-3 col-sm-10">
-            <button
-              type="submit"
-              class="bg-light btn btn-outline-primary"
-              @click="commentUse"
-            >
-              Publier !
-            </button>
+    <button type="button" class="btn btn-primary" v-on:click="show = !show">
+      Publier un commentaire
+    </button>
+    <div class="col-sm-8" v-if="!show">
+      <form>
+        <fieldset>
+          <div class="row">
+            <div class="col-sm-3 col-lg-2 hidden-xs">
+              <img
+                class="img-responsive"
+                src="https://bootdey.com/img/Content/avatar/avatar1.png"
+                alt=""
+              />
+            </div>
+            <div class="form-group col-xs-12 col-sm-9 col-lg-10 commentaire">
+              <textarea
+                class="form-control"
+                id="title"
+                placeholder="Your title"
+                required=""
+                rows="3"
+                v-model="postComment.title"
+              >
+              </textarea>
+              <textarea
+                class="form-control"
+                id="message"
+                placeholder="Your message"
+                required=""
+                rows="4"
+                v-model="postComment.content"
+              ></textarea>
+            </div>
           </div>
-        </div>
-      </div>
+        </fieldset>
+        <button
+          type="submit"
+          @click="commentUse"
+          class="bg-light btn btn-outline-primary pull-right mb-4 btn-lg"
+        >
+          Publier
+        </button>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-let iduser1 = localStorage.getItem("userChoice")
-console.log(iduser1)
-import {mapState} from "vuex";
+let iduser1 = localStorage.getItem("userChoice");
+console.log(iduser1);
+import { mapState } from "vuex";
 import axios from "axios";
 export default {
   name: "postCom",
@@ -80,19 +59,19 @@ export default {
       postComment: {
         title: "",
         content: "",
-        idcom: iduser1 ,
+        idcom: iduser1,
       },
       title1: "nc",
       content1: "nc",
       err: "",
+      show: true,
     };
   },
   computed: {
-    ...mapState(['comment']),
-      
+    ...mapState(["comment"]),
   },
-  mounted(){
-    this.$store.dispatch('commentOne')
+  mounted() {
+    this.$store.dispatch("commentOne");
   },
   methods: {
     commentUse: function () {
@@ -101,9 +80,10 @@ export default {
         axios
           .post("http://localhost:3000/comment/", this.postComment)
           .then((response) => {
-            console.log(response.data)
+            console.log(response.data);
             console.log(response);
-            console.log("commentaire envoyé à bdd");  
+            console.log("commentaire envoyé à bdd");
+            location.reload();
           })
           .catch((error) => {
             this.err = error.response;
@@ -113,40 +93,41 @@ export default {
         console.log("problème");
       }
     },
-     updateCom: function (){
-      let commentId = localStorage.getItem('userChoice')
+    updateCom: function () {
+      let commentId = localStorage.getItem("userChoice");
       axios
-      .patch("http://localhost:3000/api/comment/" + commentId,{
-
-          title: this.title1,
-          content: this.content1,
-          idcom: commentId,
-      },
-      {
-        headers:{
-          Authorization: "Bearer" + localStorage.getItem("userToken"),
-        }
-      })
-      .then((response) => console.log("success", response), location.reload())
-      .catch((error) => console.log(error))
-    },
-    deleteCom: function() {
-      const deleteComment = window.confirm('Etes vous sûr?') 
-      if (deleteComment == true){
-        let commentId = localStorage.getItem('userChoice')
-        axios.delete("http://localhost:3000/api/comment/" + commentId,{
-           headers: {
-            Authorization: "Bearer" + localStorage.getItem("userToken"),
-          }  
-        })
-        .then(() =>{
-          this.postComment,
-          console.log("Delete com fait")
-        })
+        .patch(
+          "http://localhost:3000/api/comment/" + commentId,
+          {
+            title: this.title1,
+            content: this.content1,
+            idcom: commentId,
+          },
+          {
+            headers: {
+              Authorization: "Bearer" + localStorage.getItem("userToken"),
+            },
+          }
+        )
+        .then((response) => console.log("success", response), location.reload())
         .catch((error) => console.log(error));
+    },
+    deleteCom: function () {
+      const deleteComment = window.confirm("Etes vous sûr?");
+      if (deleteComment == true) {
+        let commentId = localStorage.getItem("userChoice");
+        axios
+          .delete("http://localhost:3000/api/comment/" + commentId, {
+            headers: {
+              Authorization: "Bearer" + localStorage.getItem("userToken"),
+            },
+          })
+          .then(() => {
+            this.postComment, console.log("Delete com fait");
+          })
+          .catch((error) => console.log(error));
       }
     },
-    
   },
 };
 </script>
@@ -165,5 +146,13 @@ export default {
 .description {
   margin-top: 13px;
   margin-right: 4px;
+}
+.commentaire {
+  display: flex;
+  flex-direction: column;
+}
+#title {
+  width: 250px;
+  height: 50px;
 }
 </style>>
